e345eee94402a7b165783950ab456e7b14dd699a
edithhuangwork <edith@EdithdeMacBook-Air.local>
2026-03-05 18:00:24 +0800

    Mar5

---
+++ static_page/js/script.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+//     const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+//     toggleButton.addEventListener("click", ()=> { 
+//         mobileMenu.classList.toggle("active");
+//     });
+// });
+
+const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
