cf19ce1d1039a1f626f80a2c68443412c6879c4e
edithhuangwork <edith@EdithdeMacBook-Air.local>
2026-03-06 15:11:14 +0800

    Mar6

--- static_page/js/script.js
+++ static_page/js/script.js
@@ -1,9 +1,12 @@
-// document.addEventListener("DOMContentLoaded", ()=> {
-//     const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
-//     const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
-//     toggleButton.addEventListener("click", ()=> { 
-//         mobileMenu.classList.toggle("active");
-//     });
-// });
-
+document.addEventListener("DOMContentLoaded", ()=> {
+    const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+    const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+    toggleButton.addEventListener("click", ()=> { 
+        mobileMenu.classList.toggle("active");
+    });
+    // 加下面2行，可以click完toggle items會自動收起burger
+    mobileMenu.addEventListener("click", ()=> { 
+        mobileMenu.classList.toggle("active");
+    });
+});
 const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
